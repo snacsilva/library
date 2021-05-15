36cec1008acc991e688a8d7f4f922bdc5f51e829
Samantha Silva <samanthasilva.n@gmail.com>
2021-05-14 23:40:43 -0300

    Create users, books ans seed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+  resources :books
+  devise_for :users
+  namespace :admin do
+    resources :user
+  end
 end
---
+++ spec/views/books/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "books/index", type: :view do
+  before(:each) do
+    assign(:books, [
+      Book.create!(
+        title: "Title",
+        description: "MyText",
+        image: "Image"
+      ),
+      Book.create!(
+        title: "Title",
+        description: "MyText",
+        image: "Image"
+      )
+    ])
+  end
+
+  it "renders a list of books" do
+    render
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: "Image".to_s, count: 2
+  end
+end
